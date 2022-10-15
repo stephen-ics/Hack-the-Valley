3f48abd08ff61b5986e2f176be4c0be0f3bf1572
stephen-ics <stephenni1234@gmail.com>
2022-10-15 01:35:02 -0400

    Minor fixes Co-authored-by: bakuyy <bakuyy@users.noreply.github.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-5xl'>Homee</div>
+    <div className='flex items-center justify-center h-full bg-red-200'>
+      <div>
+        <h1 className='text-8xl text-center'><b>Title</b></h1>
+        <h1 className='text-3xl text-center'>Description</h1>
+      </div>
+    </div>
   )
 }
 
